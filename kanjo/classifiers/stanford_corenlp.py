6a2d2787a32c211321519b034fed5ba9c996d2d6
Deepu T Philip <deepu.dtp@gmail.com>
2014-05-16 12:10:56 +0530

    Return a verbose score if set

--- kanjo/classifiers/stanford_corenlp.py
+++ kanjo/classifiers/stanford_corenlp.py
@@ -5,6 +5,13 @@ from pprint import pprint
 class StanfordNLPSentiment:
     _instance = None
 
+    POLARITY_MAP = {
+        'Very positive' : 4,
+        'Positive' : 3,
+        'Neutral' : 2,
+        'Negative' : 1,
+        'Very negative' : 0
+    }
 
     def __new__(cls):
         if not cls._instance:
@@ -16,5 +23,14 @@ class StanfordNLPSentiment:
         self.server = jsonrpclib.Server(url)
 
 
-    def classify(self, text):
-        return self.server.parse(text)
+    def classify(self, text, verbose=False):
+
+        polarity = self.server.parse(text)
+        result = {}
+        result['polarity_score'] = self.POLARITY_MAP[polarity]
+        result['polarity'] = polarity
+
+        if verbose:
+            return result
+
+        return result['polarity']
